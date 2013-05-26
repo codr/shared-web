2bdb34c60e2a04f037f8c71056e5812e84283135
Eli Morris-Heft <eli.morris.heft@gmail.com>
2013-05-26 17:46:39 -0400

    This should fix bitovi/canjs.com#62.

--- _js/controls/heroDownloadCustomizer.js
+++ _js/controls/heroDownloadCustomizer.js
@@ -37,10 +37,16 @@ Bitovi.OSS.DownloadCustomizer('Bitovi.OSS.HeroDownloadCustomizer', {
 	toggleFlyout: function(open) {
 		if(open === undefined) {
 			this.isOpen = this.element.find('.customize').toggleClass('active').hasClass('active');
-			this.element.find('.customize-box').toggle();
-		} else if(open) {
+			open = this.isOpen;
+		}
+
+		if(open) {
 			this.element.find('.customize').addClass('active');
-			this.element.find('.customize-box').show();
+			var customizeBox = this.element.find('.customize-box').show();
+
+			// make customizeBox the right width
+			customizeBox.width($('#hero-download').width() - (parseInt(customizeBox.css('padding-left'), 10) + parseInt(customizeBox.css('padding-right'), 10)));
+
 			this.isOpen = true;
 		} else {
 			this.element.find('.customize').removeClass('active');
